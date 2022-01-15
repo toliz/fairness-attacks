571dd4f2a7250177686ee2bc6e33074470854be0
toliz <apospana@ece.auth.gr>
2022-01-15 16:41:53 +0100

    Added projection methods
    Added projection methods (fix unsaved file)

--- attacks/genericattack.py
+++ attacks/genericattack.py
@@ -1,3 +1,7 @@
+from turtle import right
+from attacks.anchoringattack import PoissonedDataset
+import np
+import torch
 import pandas as pd
 from attacks.datamodule import DataModule, CleanDataset
 from abc import abstractmethod
@@ -5,11 +9,8 @@ import numpy
 
 
 class GenericAttackDataModule(DataModule):
-    def __init__(self,
-                 batch_size: int,
-                 dataset: str,
-                 path: str,
-                 test_train_ratio: float = 0.2):
+
+    def __init__(self, batch_size: int, dataset: str, path: str, test_train_ratio: float = 0.2):
         super().__init__(batch_size=batch_size,
                          dataset=dataset,
                          path=path,
@@ -34,11 +35,11 @@ class GenericAttackDataModule(DataModule):
             # set up for the attack
             self.X = self.training_data[:][0]
             self.y = self.training_data[:][1]
-            self.D_a = self.X[:, self.
-                              information_dict['advantaged_column_index'] -
+            self.D_a = self.X[:,
+                              self.information_dict['advantaged_column_index'] -
                               1] == self.information_dict['advantaged_label']
-            self.D_d = self.X[:, self.
-                              information_dict['advantaged_column_index'] -
+            self.D_d = self.X[:,
+                              self.information_dict['advantaged_column_index'] -
                               1] != self.information_dict['advantaged_label']
 
             # attack the training data
@@ -81,3 +82,68 @@ class GenericAttackDataModule(DataModule):
         """
         counts = self.get_class_counts()
         return counts / numpy.sum(counts)
+    def project_onto_sphere(dataset: PoissonedDataset, radii: dict) -> PoissonedDataset:
+        """Project onto sphere method
+        
+        :dataset: the dataset with the poissoned data
+        :radii: a dictionary with the desired radius for each class
+        s
+        :return: a new dataset with anomalous points projected onto slab
+        """
+        X, Y = dataset.X.detach().clone(), dataset.Y.detach().clone()
+        classes = set(list(Y))
+
+        for c in classes:
+            # Iterate over classes and get the center and desired radius for each class
+            center = X[Y == c].mean()
+            radius = radii[c]
+
+            # Finds datatoints shifts and distances from their center
+            shifts_from_center = X[Y == c] - center
+            dists_from_center = np.linalg.norm(shifts_from_center, axis=1)
+
+            # Spot anomalous datapoints
+            anomalous_indices = dists_from_center > radius
+
+            # Project anomalous datapoints on a sphere with the desired radius
+            shifts_from_center[
+                anomalous_indices] *= radius / dists_from_center[anomalous_indices].view(-1,
+                                                                                         1)
+            X[Y == c] = shifts_from_center + center
+
+        return PoissonedDataset(X, Y)
+
+    def project_onto_slab(dataset: PoissonedDataset, radii: dict):
+        """
+        Project onto slab method - as defined in the paper "Certified Defenses for Data Poisoning
+        Attacks" (https://arxiv.org/abs/1706.03691)
+        
+        :dataset: the dataset with the poissoned data
+        :radii: a dictionary with the desired radius for each class
+        
+        :return: a new dataset with anomalous points projected onto slab
+        """
+        X, Y = dataset.X.detach().clone(), dataset.Y.detach().clone()
+        classes = set(list(Y))
+
+        # Assert the given dataset has binary labels
+        assert len(classes) == 2
+
+        # Find the vector connecting the centers of the two clusters
+        v = X[Y == classes[1]].mean() - X[Y == classes[0]].mean()
+
+        for c in classes:
+            # Iterate over classes and get the center and desired radius for each class
+            center = X[Y == c].mean()
+            radius = radii[c]
+
+            # Finds shifts and distances along the v vector
+            dists_along_v = torch.dot(X[Y == c] - center, v.T)
+            shifts_along_v = (dists_along_v - torch.clip(dists_along_v,
+                                                         -radius,
+                                                         radius)).view(1,
+                                                                       -1)
+
+            X[Y == c] -= torch.dot(shifts_along_v.T, v)
+
+        return PoissonedDataset(X, Y)
--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def run_experiment(args):
         dm.setup()
 
         # Set the model
-        model = Classifier(model=MLP(input_size=dm.get_input_size(), num_hidden=16, num_classes=dm.get_num_classes()),
+        model = Classifier(model=MLP(input_size=dm.get_input_size(),
+                                     num_hidden=16,
+                                     num_classes=dm.get_num_classes()),
                            dm=dm)
 
         # Call the model with the lowest val_loss at the end of the training
@@ -35,16 +37,21 @@ def run_experiment(args):
 
         # Set the logger
         wandb_logger = WandbLogger()
-        wandb.init(entity="angelosnal", project=args.project, job_type='train', name=args.experiment,
-                   mode=args.logger_mode, notes=args.logger_notes)
+        wandb.init(entity="angelosnal",
+                   project=args.project,
+                   job_type='train',
+                   name=args.experiment,
+                   mode=args.logger_mode,
+                   notes=args.logger_notes)
 
         # Set the trainer
-        trainer = pl.Trainer(max_epochs=args.epochs,
-                             progress_bar_refresh_rate=1,
-                             gpus=1,
-                             logger=wandb_logger,
-                             callbacks=[checkpoint_callback],
-                             )
+        trainer = pl.Trainer(
+            max_epochs=args.epochs,
+            progress_bar_refresh_rate=1,
+            gpus=1,
+            logger=wandb_logger,
+            callbacks=[checkpoint_callback],
+        )
 
         # Train and Test
         trainer.fit(model, dm)
@@ -53,7 +60,10 @@ def run_experiment(args):
 
     avg_results = get_average_results(test_results, args.num_runs)
     if args.log_average_resutls:
-        wandb.init(entity="angelosnal", project=args.project, job_type='train', name='Average: '+args.experiment,
+        wandb.init(entity="angelosnal",
+                   project=args.project,
+                   job_type='train',
+                   name='Average: ' + args.experiment,
                    notes=args.logger_notes)
         wandb.log(avg_results)
         wandb.finish()
@@ -62,45 +72,76 @@ def run_experiment(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # DataModule
-    parser.add_argument('--dataset', default='German_Credit', type=str, choices=['German_Credit', 'Drug_Consumption'],
+    parser.add_argument('--dataset',
+                        default='German_Credit',
+                        type=str,
+                        choices=['German_Credit', 'Drug_Consumption'],
                         help='Dataset name to use')
-    parser.add_argument('--path', default='data/', type=str,
+    parser.add_argument('--path',
+                        default='data/',
+                        type=str,
                         help='Path to find or save dataset')
-    parser.add_argument('--batch_size', default=64, type=int,
+    parser.add_argument('--batch_size',
+                        default=64,
+                        type=int,
                         help='Batch size to use')
 
     # Training
-    parser.add_argument('--model', default='MLP', type=str, choices=['MLP'],
+    parser.add_argument('--model',
+                        default='MLP',
+                        type=str,
+                        choices=['MLP'],
                         help='Model name to use')
-    parser.add_argument('--epochs', default=20, type=int,
+    parser.add_argument('--epochs',
+                        default=20,
+                        type=int,
                         help='Number of epochs for training')
-    parser.add_argument('--num_runs', default=5, type=int,
+    parser.add_argument('--num_runs',
+                        default=5,
+                        type=int,
                         help='Number of runs with different initializations')
 
     # Attacks
-    parser.add_argument('--attack', default='None', type=str, choices=['Anchoring', 'Influence', 'None'],
+    parser.add_argument('--attack',
+                        default='None',
+                        type=str,
+                        choices=['Anchoring', 'Influence', 'None'],
                         help='Name of the attack')
 
     # Anchoring Attack
-    parser.add_argument('--anchoring_method', default='non_random', type=str, choices=['random', 'non_random'],
+    parser.add_argument('--anchoring_method',
+                        default='non_random',
+                        type=str,
+                        choices=['random', 'non_random'],
                         help='Sampling method for anchoring attack')
-    parser.add_argument('--tau', default='0', type=float,
-                        help='')
-    parser.add_argument('--epsilon', default='1', type=float,
-                        help='')
+    parser.add_argument('--tau', default='0', type=float, help='')
+    parser.add_argument('--epsilon', default='1', type=float, help='')
 
     # Logger
-    parser.add_argument('--project', default='FACT_AI', type=str,
+    parser.add_argument('--project',
+                        default='FACT_AI',
+                        type=str,
                         help='Project name to save the logs')
-    parser.add_argument('--experiment', default='mlp_baseline', type=str,
+    parser.add_argument('--experiment',
+                        default='mlp_baseline',
+                        type=str,
                         help='Experiment name to save the logs')
-    parser.add_argument('--logger_notes', default='', type=str,
+    parser.add_argument('--logger_notes',
+                        default='',
+                        type=str,
                         help='Description of the experiment')
-    parser.add_argument('--logger_mode', default='disabled', type=str, choices=['online', 'offline', 'disabled'],
+    parser.add_argument('--logger_mode',
+                        default='disabled',
+                        type=str,
+                        choices=['online', 'offline', 'disabled'],
                         help='Mode of logger')
-    parser.add_argument('--entity', default='angelosnal', type=str,
+    parser.add_argument('--entity',
+                        default='angelosnal',
+                        type=str,
                         help='Owner\' username of wandb project')
-    parser.add_argument('--log_average_resutls', default=True, type=bool,
+    parser.add_argument('--log_average_resutls',
+                        default=True,
+                        type=bool,
                         help='Log the average results from the runs')
 
     args = parser.parse_args()
