ec1f75e93705635e8cb99a1499c1654e361e95f9
AngelosNal <angelosnal97@gmail.com>
2022-01-22 18:29:53 +0100

    implemented LinearModel

--- models/linear_model.py
+++ models/linear_model.py
@@ -1,10 +1,25 @@
-from torch import nn, Tensor
+import torch
+from torch import nn
+
 
 class LinearModel(nn.Module):
     def __init__(self):
         super().__init__()
-        
-        raise NotImplementedError()
-    
-    def forward(self, x: Tensor) -> Tensor:
-        raise NotImplementedError()
+
+    def get_params(self):
+        return tuple(self.parameters())
+
+    def get_grads(self):
+        if next(self.parameters()).grad == None:
+            return None
+        else:
+            return torch.cat([param.grad.view(-1) for param in self.parameters()])
+
+    def set_params(self, params):
+        with torch.no_grad():
+            for p1, p2 in zip(self.parameters(), params):
+                p1.copy_(p2)
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
