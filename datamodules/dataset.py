5084103fd5a723ddfc120a2ca51e6a3d5691a6dd
AngelosNal <angelosnal97@gmail.com>
2022-01-22 17:46:53 +0100

    implemented dataset

--- datamodules/dataset.py
+++ datamodules/dataset.py
@@ -6,21 +6,31 @@ from torch import Tensor
 from typing import Tuple, List
 
 
-class Dataset(torch.utils.Dataset):
-    def __init__(self, X: List[Tensor], Y: List[Tensor], adv_mask: List[Tensor]):
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, X: List[Tensor], Y: List[Tensor], adv_mask: List[bool]):
         super().__init__()
-    
+
+        self.X = torch.stack(X)
+        self.Y = torch.stack(Y)
+        self.adv_mask = adv_mask
+
     def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
-        raise NotImplementedError()
+        x = self.X[idx]
+        y = self.Y[idx]
+        return x, y
     
     def __len__(self) -> int:
-        raise NotImplementedError()
+        return len(self.X)
         
     def sample(self) -> Tuple[Tensor, Tensor]:
         raise NotImplementedError()
         
     def get_advantaged_subset(self) -> Dataset:
-        raise NotImplementedError()
+        x_adv = self.X[self.adv_mask]
+        y_adv = self.Y[self.adv_mask]
+        return Dataset([x_adv], [y_adv], [True]*len(x_adv))
     
     def get_disadvantaged_subset(self) -> Dataset:
-        raise NotImplementedError()
+        x_dis = self.X[[not adv for adv in self.adv_mask]]
+        y_dis = self.Y[[not adv for adv in self.adv_mask]]
+        return Dataset([x_dis], [y_dis], [True]*len(x_dis))
