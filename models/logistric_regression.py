742d29df0678832a0ead49516ec4a6e47b1cd021
AngelosNal <angelosnal97@gmail.com>
2022-01-22 18:40:56 +0100

    implemented LogisticRegression

--- models/logistric_regression.py
+++ models/logistric_regression.py
@@ -4,10 +4,13 @@ from linear_model import LinearModel
 
 
 class LogisticRegression(LinearModel):
-    def __init__(self):
+    def __init__(self, input_size: int):
         super().__init__()
         
-        raise NotImplementedError()
+        self.linear = nn.Sequential(
+            nn.Linear(input_size, 1),
+            nn.Sigmoid()
+        )
     
     def forward(self, x: Tensor) -> Tensor:
-        raise NotImplementedError()
+        return self.linear(x)
