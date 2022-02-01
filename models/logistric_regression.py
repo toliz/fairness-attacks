206902908e19a2a6ebf034c236419bfc569763a4
dinos <papakosk@csd.auth.gr>
2022-02-01 01:47:32 +0100

    Write linear model & logistic regression docs

--- models/logistric_regression.py
+++ models/logistric_regression.py
@@ -5,9 +5,23 @@ from .linear_model import LinearModel
 
 class LogisticRegression(LinearModel):
     def __init__(self, input_size: int):
+        """
+        Initializes a Logistic Regression model with the specified input size.
+
+        Args:
+            input_size: the model's input size
+        """
         super().__init__()
         
         self.linear = nn.Linear(input_size, 1)
     
     def forward(self, x: Tensor) -> Tensor:
+        """
+        Perform a forward pass.
+
+        Args:
+            x: the model's input
+
+        Returns: the model's output
+        """
         return self.linear(x).flatten()
